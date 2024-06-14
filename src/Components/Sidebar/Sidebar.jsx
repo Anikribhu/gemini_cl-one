95d17716fd571871d0f53f6f424c5312a11c9f0c
AnikRibhu <anikmallick28@gmail.com>
2024-06-14 21:56:22 +0530

    Document Formatted

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,55 +1,78 @@
-import React, { useState } from 'react'
-import {assets} from '../../assets/assets'
+import React, { useState } from "react";
+import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
+  const [extend, setExtend] = useState(false);
 
-    const [extend,setExtend] = useState(false)
-    
   return (
     // Sidebar
-    <div className={`sidebar max-sm:invisible max-h-[100vh] inline-flex px-4 py-4 flex-col justify-between bg-[#f0f4f9] font-custom transition-all duration-500 ${extend ? 'w-64' : 'w-20'}`}>
+    <div
+      className={`sidebar max-sm:invisible max-h-[100vh] inline-flex px-4 py-4 flex-col justify-between bg-[#f0f4f9] font-custom transition-all duration-500 ${
+        extend ? "w-64" : "w-20"
+      }`}
+    >
 
-
-
-        {/* Topbar */}
-        <div>
-            <img onClick={()=>setExtend(prev=>!prev)} className='w-6 block ml-2 cursor-pointer' src={assets.menu_icon} alt="" />
-            {/* New-Chat */}
-            <div className=' mt-12 inline-flex items-center gap-3 px-4 py-3 bg-[#dde1e7] hover:text-gray-700 cursor-pointer rounded-[50px] text-base text-gray-500'>
-                <img className='w-5' src={assets.plus_icon} alt="" />
-                {extend?<p className=''>New Chat</p>:null}
+      
+      {/* Topbar */}
+      <div>
+        <img
+          onClick={() => setExtend((prev) => !prev)}
+          className="w-6 block ml-2 cursor-pointer"
+          src={assets.menu_icon}
+          alt=""
+        />
+        {/* New-Chat */}
+        <div className=" mt-12 inline-flex items-center gap-3 px-4 py-3 bg-[#dde1e7] hover:text-gray-700 cursor-pointer rounded-[50px] text-base text-gray-500">
+          <img className="w-5" src={assets.plus_icon} alt="" />
+          {extend ? <p className="">New Chat</p> : null}
+        </div>
+        {/* Recent */}
+        {extend ? (
+          <div className="flex flex-col">
+            <p className=" mt-7 mb-5">Recent</p>
+            <div className="recent-entry  flex items-start gap-3 p-3 pr-10 rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#e2e6eb]">
+              <img className=" w-5" src={assets.message_icon} alt="" />
+              <p>What is react ....</p>
             </div>
-            {/* Recent */}
-            {extend?
-            <div className='flex flex-col'>
-                <p className=' mt-7 mb-5'>Recent</p>
-                <div className="recent-entry  flex items-start gap-3 p-3 pr-10 rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#e2e6eb]">
-                    <img className=' w-5' src={assets.message_icon} alt="" />
-                    <p>What is react ....</p>
-                </div>
-            </div>
-            :null
-        }
-        </div>
+          </div>
+        ) : null}
+      </div>
 
-
-        {/* BottomBar */}
-        <div className='flex flex-col'>
+      {/* BottomBar */}
+      <div className="flex flex-col">
         <div className="bottom-item recent-entry flex items-start gap-3 p-3 pr-10 rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#e2e6eb]">
-          <img className='w-6' src={assets.question_icon} alt="" />
-          <p className={`transition-opacity duration-500 ${extend ? 'opacity-100' : 'opacity-0'}`}>Help</p>
+          <img className="w-6" src={assets.question_icon} alt="" />
+          <p
+            className={`transition-opacity duration-500 ${
+              extend ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            Help
+          </p>
         </div>
         <div className="bottom-item recent-entry flex items-start gap-3 p-3 pr-10 rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#e2e6eb]">
-          <img className='w-6' src={assets.history_icon} alt="" />
-          <p className={`transition-opacity duration-500 ${extend ? 'opacity-100' : 'opacity-0'}`}>Activity</p>
+          <img className="w-6" src={assets.history_icon} alt="" />
+          <p
+            className={`transition-opacity duration-500 ${
+              extend ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            Activity
+          </p>
         </div>
         <div className="bottom-item recent-entry flex items-start gap-3 p-3 pr-10 rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#e2e6eb]">
-          <img className='w-6' src={assets.setting_icon} alt="" />
-          <p className={`transition-opacity duration-500 ${extend ? 'opacity-100' : 'opacity-0'}`}>Setting</p>
+          <img className="w-6" src={assets.setting_icon} alt="" />
+          <p
+            className={`transition-opacity duration-500 ${
+              extend ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            Setting
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
